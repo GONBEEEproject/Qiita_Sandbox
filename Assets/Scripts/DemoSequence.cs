bf2f3097a40698fd3bd9ce426c8207cc35555ab7
GONBEEE_project_MPlexPC <gonbeee.project@gmail.com>
2019-09-24 18:18:32 +0900

    FirstCommit

---
+++ Assets/Scripts/DemoSequence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DemoSequence : MonoBehaviour
+{
+    [SerializeField]
+    private Transform moveTarget;
+    [SerializeField]
+    private Transform startMark, endMark;
+
+    private void Start()
+    {
+        moveTarget.position = startMark.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        //moveTarget.DOMove(endMark.position, 2.0f);
+
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(moveTarget.DOMove(endMark.position, 2.0f));
+        sequence.Append(moveTarget.DOMove(startMark.position, 2.0f));
+
+        sequence.Play();
+    }
+}
